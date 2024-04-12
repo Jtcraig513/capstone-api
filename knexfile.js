9413fda5a3712c6c7cc358d9b8cc729b72c60fbc
Jake Craig <jtcraig513@gmail.com>
2024-04-12 15:41:16 -0400

    Fixing Deployment

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 module.exports = {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1",
+    host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
