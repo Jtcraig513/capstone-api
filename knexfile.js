bcea99685ae3f1d4e9557122e94c0d3eba2cfced
Jake Craig <jtcraig513@gmail.com>
2024-04-12 15:32:38 -0400

    Minor adjustment

--- knexfile.js
+++ knexfile.js
@@ -10,9 +10,9 @@ module.exports = {
   client: "mysql2",
   connection: {
     host: "127.0.0.1",
-    database: process.env.DB_LOCAL_DBNAME,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     charset: "utf8",
   },
 };
